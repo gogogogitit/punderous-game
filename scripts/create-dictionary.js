e9f176a3de8c5956900931d1717fb32cc15623b0
Michael Katz <mjkultra@gmail.com>
2024-11-11 16:31:37 -0500

    add apostrophe words

--- scripts/create-dictionary.js
+++ scripts/create-dictionary.js
@@ -7,6 +7,11 @@ const gzip = promisify(zlib.gzip);
 const top500Words = [
   'I', 'a', 'of', 'to', 'in', 'it', 'is', 'be', 'as', 'at', 'so', 'we', 'he', 'by', 'or', 'on', 'do', 'if', 'me', 'my', 'up', 'an', 'go', 'no', 'us', 'am',
   'the', 'and', 'that', 'have', 'for', 'not', 'with', 'you', 'this', 'but', 'his', 'from', 'they', 'say', 'her', 'she', 'will', 'one', 'all', 'would', 'there', 'their', 'what', 'out', 'about', 'who', 'get', 'which', 'when', 'make', 'can', 'like', 'time', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'day', 'most', 'us',
+  "can't",
+  "don't",
+  "won't",
+  "isn't",
+  "aren't"
   // ... (add the rest of the words here)
 ];
 
