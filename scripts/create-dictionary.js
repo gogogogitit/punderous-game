4bb629e608007405d56c54f8050dc16f59d01071
Michael Katz <mjkultra@gmail.com>
2024-11-13 09:20:19 -0500

    apostrophe word logic update

--- scripts/create-dictionary.js
+++ scripts/create-dictionary.js
@@ -5,7 +5,6 @@ const path = require('path');
 const top500Words = [
   'I', 'a', 'of', 'to', 'in', 'it', 'is', 'be', 'as', 'at', 'so', 'we', 'he', 'by', 'or', 'on', 'do', 'if', 'me', 'my', 'up', 'an', 'go', 'no', 'us', 'am',
   'the', 'and', 'that', 'have', 'for', 'not', 'with', 'you', 'this', 'but', 'his', 'from', 'they', 'say', 'her', 'she', 'will', 'one', 'all', 'would', 'there', 'their', 'what', 'out', 'about', 'who', 'get', 'which', 'when', 'make', 'can', 'like', 'time', 'just', 'him', 'know', 'take', 'people', 'into', 'year', 'your', 'good', 'some', 'could', 'them', 'see', 'other', 'than', 'then', 'now', 'look', 'only', 'come', 'its', 'over', 'think', 'also', 'back', 'after', 'use', 'two', 'how', 'our', 'work', 'first', 'well', 'way', 'even', 'new', 'want', 'because', 'any', 'these', 'give', 'yours', 'years', 'ours', 'day', 'most', 'us',
-  "can't", "don't", "won't", "isn't", "aren't"
   // ... (add the rest of the words here)
 ];
 
@@ -99,43 +98,54 @@ const punWords = [
   "snake", "building", "house", "boa", "constructor",
   "bear", "never", "wants", "grow", "peter", "panda",
   "cow", "legs", "ground", "beef"
-
 ];
 
+// Extended list of contractions
 const contractions = [
-    "cannot", "didn't",
-    "don't", "do not",
-    "won't", "will not",
-    "isn't", "is not",
-    "aren't", "are not",
-    "it's", "it is",
-    "I'm", "I am",
-    "you're", "you are",
-    "they are",
-    "we're", "we are",
-    "he's", "he is",
-    "she's", "she is",
-    "that's", "that is",
-    "who's", "who is",
-    "what's", "what is",
-    "where's", "where is",
-    "when's", "when is",
-    "why's", "why is",
-    "how's", "how is"
-  ];
+  "can't", "cannot", "couldn't", "could not", "didn't", "did not",
+  "don't", "do not", "doesn't", "does not", "won't", "will not",
+  "isn't", "is not", "aren't", "are not", "wasn't", "was not",
+  "weren't", "were not", "hasn't", "has not", "haven't", "have not",
+  "hadn't", "had not", "it's", "it is", "I'm", "I am", "you're", "you are",
+  "they're", "they are", "we're", "we are", "he's", "he is", "she's", "she is",
+  "that's", "that is", "who's", "who is", "what's", "what is",
+  "where's", "where is", "when's", "when is", "why's", "why is",
+  "how's", "how is", "let's", "let us", "there's", "there is",
+  "here's", "here is", "ain't", "am not", "i'll", "i will", "you'll", "you will",
+  "he'll", "he will", "she'll", "she will", "we'll", "we will",
+  "they'll", "they will", "i'd", "i would", "you'd", "you would",
+  "he'd", "he would", "she'd", "she would", "we'd", "we would",
+  "they'd", "they would", "i've", "i have", "you've", "you have",
+  "we've", "we have", "they've", "they have", "who've", "who have",
+  "would've", "would have", "should've", "should have", "could've", "could have",
+  "might've", "might have", "must've", "must have"
+];
+
+// Common nouns for possessive forms
+const commonNouns = [
+  "cat", "dog", "house", "car", "book", "friend", "family", "world", "country", "city",
+  "man", "woman", "child", "parent", "teacher", "student", "doctor", "patient",
+  "bird", "tree", "flower", "river", "mountain", "ocean", "sun", "moon", "star",
+  "computer", "phone", "television", "radio", "movie", "music", "food", "water",
+  "air", "earth", "fire", "time", "day", "night", "week", "month", "year"
+];
+
+// Generate possessive forms
+const possessives = commonNouns.map(noun => `${noun}'s`);
 
 // Combine all words, convert to lowercase, and remove duplicates
 const allWords = new Set([
-    ...top500Words, 
-    ...punWords, 
-    ...contractions
-  ].map(word => word.toLowerCase()));
-  
-  // Convert Set to sorted array
-  const compressedDictionary = Array.from(allWords).sort();
-  
-  // Save the compressed dictionary
-  const outputPath = path.join(__dirname, '..', 'public', 'compressed-dictionary.json');
-  fs.writeFileSync(outputPath, JSON.stringify(compressedDictionary));
-  
-  console.log('Compressed dictionary with top words, pun words, and contractions created successfully in public folder.');
+  ...top500Words, 
+  ...punWords, 
+  ...contractions,
+  ...possessives
+].map(word => word.toLowerCase()));
+
+// Convert Set to sorted array
+const compressedDictionary = Array.from(allWords).sort();
+
+// Save the compressed dictionary
+const outputPath = path.join(__dirname, '..', 'public', 'compressed-dictionary.json');
+fs.writeFileSync(outputPath, JSON.stringify(compressedDictionary));
+
+console.log('Compressed dictionary with top words, pun words, contractions, and possessives created successfully in public folder.');
