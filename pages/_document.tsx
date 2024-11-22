1392f371dcc479624bcad6cca4649e33921815ee
Michael Katz <mjkultra@gmail.com>
2024-11-22 00:09:42 -0500

    FB social media image sharing fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
+    const domain = process.env.NEXT_PUBLIC_DOMAIN || 'https://punderous.com';
+    
     return (
       <Html lang="en">
         <Head>
@@ -17,10 +19,11 @@ class MyDocument extends Document {
           <meta property="fb:app_id" content="462159136491139" />
           <meta property="og:title" content="Punderous™ - A Pun-Filled Word Game!" />
           <meta property="og:description" content="Play Punderous™ - a pun-filled word game where we ask the questions and you guess the puns!" />
-          <meta property="og:url" content="https://punderous.com" />
+          <meta property="og:url" content={domain} />
           <meta property="og:site_name" content="Punderous™" />
           <meta property="og:locale" content="en_US" />
-          <meta property="og:image" content="https://punderous.com/og-image.png" />
+          <meta property="og:image" content={`${domain}/images/og-image.png`} />
+          <meta property="og:image:type" content="image/png" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="630" />
           <meta property="og:image:alt" content="Punderous™ - A Pun-Filled Word Game!" />
@@ -31,7 +34,7 @@ class MyDocument extends Document {
           <meta name="twitter:creator" content="@PunderousGame" />
           <meta name="twitter:title" content="Punderous™ - A Pun-Filled Word Game!" />
           <meta name="twitter:description" content="Play Punderous™ - a pun-filled word game where we ask the questions and you guess the puns!" />
-          <meta name="twitter:image" content="https://punderous.com/twitter-image.png" />
+          <meta name="twitter:image" content={`${domain}/images/twitter-image.png`} />
 
           {/* JSON-LD Structured Data */}
           <script
@@ -42,9 +45,9 @@ class MyDocument extends Document {
                 "@type": ["WebApplication", "Game"],
                 "name": "Punderous™",
                 "description": "Play Punderous™ - a pun-filled word game where we ask the questions and you guess the puns!",
-                "url": "https://punderous.com",
-                "image": "https://punderous.com/og-image.png",
-                "screenshot": "https://punderous.com/og-image.png",
+                "url": domain,
+                "image": `${domain}/images/og-image.png`,
+                "screenshot": `${domain}/images/og-image.png`,
                 "applicationCategory": "GameApplication",
                 "gameType": "Word Game",
                 "genre": "Puzzle",
@@ -66,11 +69,17 @@ class MyDocument extends Document {
             }}
           />
 
+          {/* SEO Tags */}
+          <meta name="robots" content="index, follow" />
+          <meta name="googlebot" content="index, follow, max-video-preview:-1, max-image-preview:large, max-snippet:-1" />
+          <link rel="canonical" href={domain} />
+          <meta name="format-detection" content="telephone=no, address=no, email=no" />
+
           {/* Favicon and App Icons */}
           <link rel="icon" href="/favicon.ico" sizes="32x32" />
-          <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
-          <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png" sizes="180x180" />
+          <link rel="icon" href="/images/favicon-16x16.png" sizes="16x16" type="image/png" />
+          <link rel="icon" href="/images/favicon-32x32.png" sizes="32x32" type="image/png" />
+          <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" sizes="180x180" />
           <link rel="manifest" href="/site.webmanifest" crossOrigin="use-credentials" />
         </Head>
         <body>
