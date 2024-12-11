c660d1330022cfd4667d77310eaeb6e587b6b5e0
Michael Katz <mjkultra@gmail.com>
2024-12-11 15:35:40 -0500

    indexing error and favicon updates

--- next.config.js
+++ next.config.js
@@ -34,6 +34,19 @@ const nextConfig = {
             value: 'index, follow'
           }
         ]
+      },
+      {
+        source: '/favicon.ico',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          },
+          {
+            key: 'Content-Type',
+            value: 'image/x-icon'
+          }
+        ]
       }
     ]
   }
